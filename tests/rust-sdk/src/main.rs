ea0d4eeb0868b1978dfab6f4d64f47823c739708
musitdev <philippe.delrieu@free.fr>
2024-06-15 15:38:52 +0200

    Try to add Alice and Bod account scnearios

--- tests/rust-sdk/src/main.rs
+++ tests/rust-sdk/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use aptos_sdk::coin_client::CoinClient;
+use aptos_sdk::coin_client::TransferOptions;
 use aptos_sdk::crypto::ed25519::Ed25519PrivateKey;
 use aptos_sdk::crypto::ed25519::Ed25519PublicKey;
 use aptos_sdk::crypto::ValidCryptoMaterialStringExt;
@@ -22,6 +23,7 @@ use aptos_sdk::{
         LocalAccount,
     },
 };
+use std::str::FromStr;
 
 const PRIVATE_KEY: &str = "0xcb1fe7df72aff4a114d2bff60ecce2172f342b66cd5dafb2b8844b25e29b8d58";
 //const PUBLIC_KEY: &str = "0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92";
@@ -38,7 +40,7 @@ async fn main() -> Result<()> {
     let chain_id = ChainId::new(CHAIN_ID);
 
     // Build a local representation of an account.
-    let account = LocalAccount::new(account_address, private_key, 0);
+    let mut account = LocalAccount::new(account_address, private_key, 0);
 
     let faucet_client = FaucetClient::new(FAUCET_URL.parse()?, RPC_URL.parse()?);
     faucet_client
@@ -46,9 +48,16 @@ async fn main() -> Result<()> {
         .await
         .unwrap();
 
-    // Build an API client.
+    // Create initial account and data.
     let client = Client::new(RPC_URL.parse()?);
     let coin_client = CoinClient::new(&client);
+    let alice = LocalAccount::generate(&mut rand::rngs::OsRng);
+    let alice_address = alice.address();
+    let mut bob = LocalAccount::generate(&mut rand::rngs::OsRng);
+    let bob_address = bob.address();
+    faucet_client.create_account(alice.address()).await?;
+    faucet_client.fund(alice.address(), 100_000_000).await?;
+    faucet_client.create_account(bob.address()).await?;
 
     let balance = coin_client
         .get_account_balance(&account_address)
@@ -58,21 +67,25 @@ async fn main() -> Result<()> {
     // let balance = client.get_account_balance(account_address).await;
     println!("balance:{balance:?}",);
 
-    //get account sequence numner
+    //get account sequence number
     let account_rpc = client.get_account(account_address).await.unwrap();
     let mut sequence_number = account_rpc.inner().sequence_number;
     println!("sequence_number: {sequence_number:?}",);
 
-    // # TestCoinsV1
-    // echo "Mint BTC TestCoinsV1"
-
-    // echo "Initialize TestCoinsV1"
-    // aptos move run --function-id ${SwapDeployer}::TestCoinsV1::initialize --assume-yes
-    let tx_result = run_function(
-    	"0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::initialize",
-    	&account, &client, sequence_number, chain_id, vec![], vec![]).await?;
-    println!("RESULTTTTTTT Mint tx_receipt_data: {tx_result:?}",);
-    sequence_number += 1;
+    //aptos move run --function-id ${SwapDeployer}::TestCoinsV1::initialize --assume-yes
+    // done in script because must be done one time.
+    // let tx_result = run_function(
+    //     &"0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::initialize",
+    //     &account,
+    //     &client,
+    //     sequence_number,
+    //     chain_id,
+    //     vec![],
+    //     vec![],
+    // )
+    // .await?;
+    // println!("RESULTTTTTTT Alice register tx_receipt_data: {tx_result:?}",);
+    // sequence_number += 1;
 
     // echo "Mint USDT TestCoinsV1"
     // aptos move run --function-id ${SwapDeployer}::TestCoinsV1::mint_coin \
@@ -84,25 +97,105 @@ async fn main() -> Result<()> {
         name: Identifier::new("USDT").unwrap(),
         type_args: vec![],
     }));
-    let tx_result = run_function("0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::mint_coin", 
-    	&account, &client, sequence_number, chain_id
-    	, vec![tytag], vec![bcs::to_bytes(&account_address).unwrap(), bcs::to_bytes(&(20000000000000000 as u64)).unwrap()]).await?;
+    let tx_result = run_function(
+    	"0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::mint_coin", 
+    	&account,
+    	&client,
+    	sequence_number,
+    	chain_id,
+    	vec![tytag],
+    	vec![bcs::to_bytes(&account_address).unwrap(), bcs::to_bytes(&(20000000000000000 as u64)).unwrap()]
+    )
+    .await?;
     println!("RESULTTTTTTT Mint tx_receipt_data: {tx_result:?}",);
     sequence_number += 1;
 
-    // aptos move run --function-id ${SwapDeployer}::TestCoinsV1::mint_coin \
-    // --args address:${SwapDeployer} u64:2000000000000 \
-    // --type-args ${SwapDeployer}::TestCoinsV1::BTC --assume-yes
-    let tytag = TypeTag::Struct(Box::new(StructTag {
-        address: account_address,
-        module: Identifier::new("TestCoinsV1").unwrap(),
-        name: Identifier::new("BTC").unwrap(),
-        type_args: vec![],
-    }));
-    let tx_result = run_function("0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::mint_coin", 
-    	&account, &client, sequence_number, chain_id
-    	, vec![tytag], vec![bcs::to_bytes(&account_address).unwrap(), bcs::to_bytes(&(2000000000000 as u64)).unwrap()]).await?;
-    println!("RESULTTTTTTT Mint tx_receipt_data: {tx_result:?}",);
+    //Deposit some coin to alice
+    //coin::deposit<BTC>(account, coin);
+    let tytag = TypeTag::from_str(
+        "0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::USDT",
+    )
+    .unwrap();
+    let tx_result = run_function(
+        "0x1::coin::deposit",
+        &account,
+        &client,
+        sequence_number,
+        chain_id,
+        vec![tytag],
+        vec![bcs::to_bytes(&alice_address).unwrap()],
+    )
+    .await;
+    sequence_number += 1;
+    println!("RESULTTTTTTT coin depo test result: {tx_result:?}",);
+
+    //Transfer USD to Alice
+    let mut transfer_coin = TransferOptions::default();
+    transfer_coin.coin_type =
+        "0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::USDT";
+    account.set_sequence_number(sequence_number);
+    let pending_txn = coin_client
+        .transfer(&mut account, alice.address(), 1_000, Some(transfer_coin))
+        .await?;
+    let tr_result = client.wait_for_transaction(&pending_txn).await;
+    println!("RESULTTTTTTT transfer result: {tr_result:?}",);
+    sequence_number += 1;
+
+    //Bob try to mint coin
+    //Generate a Transaction expired skip
+    // let tytag = TypeTag::Struct(Box::new(StructTag {
+    //     address: account_address,
+    //     module: Identifier::new("TestCoinsV1").unwrap(),
+    //     name: Identifier::new("USDT").unwrap(),
+    //     type_args: vec![],
+    // }));
+    // let tx_result = run_function("0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::mint_coin",
+    // 	&bob, &client, sequence_number, chain_id
+    // 	, vec![tytag], vec![bcs::to_bytes(&bob_address).unwrap(), bcs::to_bytes(&(20000000000000000 as u64)).unwrap()]).await?;
+    // println!("RESULTTTTTTT Bob Mint tx_receipt_data: {tx_result:?}",);
+    // sequence_number += 1;
+
+    //Bob try to tranfer some USD to Alice. Generate INSUFFICIENT_BALANCE_FOR_TRANSACTION_FEE error
+    let mut transfer_coin = TransferOptions::default();
+    transfer_coin.coin_type =
+        "0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::USDT";
+    let tr_result = coin_client
+        .transfer(&mut bob, alice.address(), 1_000, Some(transfer_coin))
+        .await;
+    sequence_number += 1;
+    println!("RESULTTTTTTT Bob bad transfer result: {tr_result:?}",);
+    assert!(
+        tr_result.is_err(),
+        "Bob INSUFFICIENT_BALANCE_FOR_TRANSACTION_FEE allowed."
+    );
+
+    //Alice tranfer some fund to Bod but sign with wrong account
+    let tytag = TypeTag::from_str(
+        "0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::USDT",
+    )
+    .unwrap();
+    let tx_result = run_function(
+        "0x1::aptos_account::transfer",
+        &alice,
+        &client,
+        sequence_number,
+        chain_id,
+        vec![tytag],
+        vec![
+            bcs::to_bytes(&bob_address).unwrap(),
+            bcs::to_bytes(&(100 as u64)).unwrap(),
+        ],
+    )
+    .await?;
+    println!("RESULTTTTTTT transfer2 result: {tx_result:?}",);
+    let mut transfer_coin = TransferOptions::default();
+    transfer_coin.coin_type =
+        "0xd82405d9faa256840ff6a8fe78d28d3f43581b1d34aa7f78476f4ce7e47a9e92::TestCoinsV1::USDT";
+    account.set_sequence_number(sequence_number);
+    let txn_hash = coin_client
+        .transfer(&mut account, alice.address(), 1_000, Some(transfer_coin))
+        .await?;
+    println!("RESULTTTTTTT transfer result: {txn_hash:?}",);
     sequence_number += 1;
 
     Ok(())
@@ -128,14 +221,15 @@ async fn run_function(
         .sender(account.address())
         .sequence_number(sequence_number)
         .build();
-    println!("raw_tx:{raw_tx:?}",);
+    //    println!("raw_tx:{raw_tx:?}",);
 
     let signed_transaction = account.sign_transaction(raw_tx);
-    println!("signed_transaction:{signed_transaction:?}",);
+    //    println!("signed_transaction:{signed_transaction:?}",);
 
-    let tx_receipt_data = client.submit_and_wait_bcs(&signed_transaction).await?;
-    println!("RESULTTTTTTT Mint tx_receipt_data: {tx_receipt_data:?}",);
-    //TODO process result
+    let pending_txn = client.submit(&signed_transaction).await?.into_inner();
+    let tx_receipt_data = client.wait_for_transaction_bcs(&pending_txn).await?;
+    //    println!("RESULTTTTTTT run_function: {tx_receipt_data:?}",);
+
     let TransactionInfo::V0(tx_info) = tx_receipt_data.into_inner().info;
 
     if let ExecutionStatus::Success = tx_info.status() {
